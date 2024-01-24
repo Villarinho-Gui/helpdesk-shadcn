f4e88c74cb5168d78f6cb45d23eaca50e278497f
Guilherme Villarinho <Guivillarinho15@gmail.com>
2024-01-24 08:52:10 -0300

    refactor: Adicionando url pela variável de ambiente

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,9 @@
 import axios from 'axios'
 
+import { env } from '@/env'
+
 export default axios.create({
-  baseURL: 'http://localhost:3545',
+  baseURL: env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
