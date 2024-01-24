24a82f53f19d06ed29183a64b484d472c6fbdafb
Guilherme Villarinho <Guivillarinho15@gmail.com>
2024-01-24 09:24:47 -0300

    refactor: Atualizando importações

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
+import { QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from 'react-query'
 
 import { App } from './App.tsx'
-
-const client = new QueryClient()
+import { queryClient } from './lib/react-query.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={client}>
+    <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
   </React.StrictMode>,
