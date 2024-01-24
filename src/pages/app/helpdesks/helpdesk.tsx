6fb1275902de1520183c976e311515ca8168ac32
Guilherme Villarinho <Guivillarinho15@gmail.com>
2024-01-24 11:19:48 -0300

    docs: Atualizando nomes

--- src/pages/app/helpdesks/helpdesk.tsx
+++ src/pages/app/helpdesks/helpdesk.tsx
@@ -1,37 +1,76 @@
-import { useQuery } from '@tanstack/react-query'
-import { Helmet } from 'react-helmet-async'
+import { MessageSquareShare } from 'lucide-react'
 
-import { getHelpDesk } from '@/api/get-helpdesk'
-import { HelpDeskFilter } from '@/components/helpdesk-filter'
+import { AuthorInfoCard } from '@/components/author-info-card'
+import { FileCard } from '@/components/file-card'
+import { Badge } from '@/components/ui/badge'
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { Dialog, DialogTrigger } from '@/components/ui/dialog'
 
-import { HelpDeskDetail } from './helpdesk-detail'
+interface FileProps {
+  id: string
+  filename: string
+  mimetype: string
+}
+interface HelpDeskDetailsProps {
+  id: string
+  author: string
+  title: string
+  category: string
+  description: string
+  file?: FileProps[]
+}
 
-export function HelpDesks() {
-  const { data } = useQuery({
-    queryKey: ['helpdesk'],
-    queryFn: getHelpDesk,
-    refetchOnWindowFocus: false,
-  })
+export function HelpDeskDetail({
+  id,
+  author,
+  category,
+  description,
+  title,
+  file,
+}: HelpDeskDetailsProps) {
   return (
-    <>
-      <Helmet title="HelpDesks" />
-      <div className="sticky left-0 right-0 top-14 flex h-20 justify-center space-y-3">
-        <HelpDeskFilter />
-      </div>
-      {data &&
-        data.length > 0 &&
-        data.map((helpdesk) => (
-          <div className="flex flex-col items-center" key={helpdesk.id}>
-            <HelpDeskDetail
-              id={helpdesk.id}
-              author={helpdesk.user.name}
-              category={helpdesk.category}
-              description={helpdesk.description}
-              title={helpdesk.title}
-              file={helpdesk.files}
-            />
-          </div>
-        ))}
-    </>
+    <Card className="mr-2 mt-2 h-max w-[760px]">
+      <CardHeader className="flex flex-row justify-between">
+        <div className="flex flex-col gap-2">
+          <CardTitle>{title}</CardTitle>
+          <CardDescription>
+            <AuthorInfoCard name={author} />
+          </CardDescription>
+        </div>
+        <span className="text-sm text-muted-foreground">há 5 horas</span>
+      </CardHeader>
+      <CardContent>
+        <div className="space-y-4">
+          <Badge>{category}</Badge>
+          <p className="space-y-2">{description}</p>
+        </div>
+
+        {file &&
+          file.length > 0 &&
+          file.map((file) => (
+            <div className="flex gap-2" key={file.id}>
+              <FileCard filename={file.filename} mimetype={file.mimetype} />
+            </div>
+          ))}
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="ghost">
+              <MessageSquareShare />
+            </Button>
+          </DialogTrigger>
+        </Dialog>
+        <Badge variant="secondary">{id}</Badge>
+      </CardFooter>
+    </Card>
   )
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
-import { HelpDesks } from './pages/app/helpdesks/helpdesk'
+import { HelpDesks } from './pages/app/helpdesks/helpdesk-list'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
 
